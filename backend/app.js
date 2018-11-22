79f5bf36e2e68de30eaab0c79d8c027d9ac3c1a4
Gabor Monostori <monogab@gmail.com>
2018-11-22 15:19:36 +0100

    call between frontend-backend

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,14 @@ const csv = require('csv-parser');
 const app = express();
 const port = 3000;
 
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
 app.post('/api/csv2json', (req, res) => {
 
   const form = new formidable.IncomingForm();
--- frontend-react/csv-upload.js
+++ frontend-react/csv-upload.js
@@ -5,9 +5,50 @@ const e = React.createElement;
 class CsvUploadComponent extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { liked: false };
+        this.state = { tableheader: null, tabledata : null };
     }
+    convert(event) {
 
+        if (event.target.files && event.target.files.length > 0) {
+            const file = event.target.files[0];
+
+
+
+            const formData = new FormData();
+            formData.append('file', file, file.name);
+            // this.loadTable(formData);
+            this.fetchTable(formData);
+
+
+        }
+
+    }
+    fetchTable(formData){
+
+        fetch(`http://localhost:3000/api/csv2json`, {
+                method: 'POST',
+                body: formData // This is your file object
+             })
+            .then(response => response.json())
+            .then(
+                result => {
+                    console.log(result);
+                    this.setState({tableheader: result.table.header, tabledata : result.table.lines});
+
+            })
+            .catch(
+                error => console.log(error) // Handle the error response object
+            );
+    };
+
+    // this.http.post(`/api/csv2json`, formdata).subscribe(  (result) => {
+    //
+    //         if (result.table) {
+    //
+    //         }
+    //
+    //     });
+    // }
     render() {
         if (this.state.liked) {
             return 'You liked this.';
@@ -20,7 +61,7 @@ class CsvUploadComponent extends React.Component {
                 className: "btn btn-primary",
                 type: "file",
                 name: "filetoupload",
-                onChange: () => this.setState({ liked: true }),
+                onChange: this.convert.bind(this),
             })
 
         );
