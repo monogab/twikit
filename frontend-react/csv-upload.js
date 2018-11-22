72c8b99b203fa5dbc13c244bd230dd1c09a1334f
Gabor Monostori <monogab@gmail.com>
2018-11-22 16:50:17 +0100

    making the table

--- frontend-react/csv-upload.js
+++ frontend-react/csv-upload.js
@@ -2,6 +2,7 @@
 
 const e = React.createElement;
 
+
 class CsvUploadComponent extends React.Component {
     constructor(props) {
         super(props);
@@ -11,8 +12,6 @@ class CsvUploadComponent extends React.Component {
 
         if (event.target.files && event.target.files.length > 0) {
             const file = event.target.files[0];
-
-
 
             const formData = new FormData();
             formData.append('file', file, file.name);
@@ -41,17 +40,32 @@ class CsvUploadComponent extends React.Component {
             );
     };
 
-    // this.http.post(`/api/csv2json`, formdata).subscribe(  (result) => {
-    //
-    //         if (result.table) {
-    //
-    //         }
-    //
-    //     });
-    // }
     render() {
-        if (this.state.liked) {
-            return 'You liked this.';
+        let Table = null;
+        if (this.state.tableheader) {
+            const TableTh = this.state.tableheader.map((item) => React.createElement("th", null, item));
+
+            const TableCells = this.state.tabledata.map((line) => {
+                return React.createElement("tr",
+                    null,
+                    line.map((cell) => React.createElement("td", null, cell)));
+            })
+            // Table = [TableTh,TableCells];
+            Table = React.createElement(
+                "div",
+                { className: "container mt-4" },
+                React.createElement(
+                    "table",
+                    { className: "table table-striped table-bordered table-hover" },
+                    React.createElement(
+                        "thead",
+                        null,
+                        React.createElement("tr", null, TableTh)
+                    ),
+                    React.createElement("tbody", null, TableCells)
+                )
+            );
+
         }
 
         return e(
@@ -62,7 +76,8 @@ class CsvUploadComponent extends React.Component {
                 type: "file",
                 name: "filetoupload",
                 onChange: this.convert.bind(this),
-            })
+            }),
+            Table
 
         );
     }
